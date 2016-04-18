ef4421c9d2171fe60c1c453bd4f120e540939e30
v613 <cebandima@ymail.com>
2016-04-18 20:37:02 +0300

    Prepare workplace for new files

--- js/index.js
+++
@@ -1,9 +0,0 @@
-		function inArray(table, value)
-			{var i=0;
-				for(;i<table.length;i++)
-					if(table[i]==value)
-						return true;
-				return false;
-			}
-		function send()
-			{window.location.href = "view.php?dis=" + Dis;}
--- js/modal.js
+++
@@ -1,5 +0,0 @@
-		function modal(value)
-			{
-				X = document.getElementById(value);
-				X.style.visibility = (X.style.visibility == "visible") ? "hidden" : "visible";
-			}
