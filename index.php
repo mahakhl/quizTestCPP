6b14685b30d51e84ca80553a445d3aad37ab637b
v613 <cebandima@ymail.com>
2016-05-02 11:28:47 +0300

    Login Page

---
+++ index.php
@@ -0,0 +1,60 @@
+<?
+if (isset($_POST['submit']))
+    {
+      $Email = $_POST['E'];
+      $User = $_POST['U'];
+      include "blocks/connect";
+      $Q=mysql_query("SELECT grad FROM People WHERE paswd='$Email' and nume='$User'");
+        if($Q === FALSE)die(mysql_error());
+      $q=mysql_fetch_row($Q);
+      session_start();
+      $_SESSION["E"] = "$Email";
+      $_SESSION["U"] = "$User";
+      switch ($q[0]) {
+        case 'student': header('Location: view.php');break;
+        case 'prof': header('Location: administration');break;
+      }
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta charset="utf-8">
+		<title>quizCPP Login</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/styles.css" rel="stylesheet">
+	</head>
+	<body>
+<!--login modal-->
+<div id="loginModal" class="modal show" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog">
+  <div class="modal-content">
+      <div class="modal-header">
+          <h1 class="text-center">Login</h1>
+      </div>
+      <div class="modal-body">
+          <form class="form col-md-12 center-block" name="loginForm" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            <div class="form-group">
+              <input name="E" type="password" class="form-control input-lg" placeholder="Email">
+            </div>
+            <div class="form-group">
+              <input name="U" type="text" class="form-control input-lg" placeholder="Nume">
+            </div>
+            <div class="form-group">
+              <button type="submit" name="submit" class="btn btn-primary btn-lg btn-block">Autentificare</button>
+            </div>
+          </form>
+      </div>
+      <div class="modal-footer">  
+      </div>
+  </div>
+  </div>
+</div>
+
+	<!-- script references -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
